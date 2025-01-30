0c89ae029316dbe3c13a106418aa0904762bca0a
Fabrício Oliveira de Sousa <fabricioodsousa1@gmail.com>
2025-01-30 16:43:47 -0300

    starting main

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const playlist = document.getElementById('playlist')
 const newPlaylist = document.getElementById('new-playlist')
 
 playlist.addEventListener('click', function(){ 
-    newPlaylist.style.display = newPlaylist.style.display === 'none' ? 'flex' : 'none';
+    newPlaylist.style.display = newPlaylist.style.display === 'flex' ? 'none' : 'flex';
 })
